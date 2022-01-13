3592cce529bb6aedc4890ec0cda9d266855cba5d
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-01-13 14:09:48 +0100

    step-29

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
